cd2652c279267aa7522886473bde82fe4d117d14
Diiii <andyappdev@yahoo.com>
2020-12-02 12:55:56 +0100

    Update client.py
    The input dataframe is now refreshed and received each time before the algorithm runs

--- client.py
+++ client.py
@@ -2,19 +2,20 @@ import zmq
 import pandas_zmq
 import pandas as pd
 import hashlib
+import time
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5555")
 socket.setsockopt(zmq.SUBSCRIBE, ''.encode('utf-8'))
-df = pandas_zmq.recv_dataframe(socket)
 
 k = 3
 charsToKeep = 2
 identifierCol = 'MAC address'
+colB = 'MAC address'
 
 def dataPreProcessing(targetColumn):
-    # Performing data pseudonym (sha256 hash) and data truncation. Last n bits are kept.
+    # Performing data pseudonym (sha256 hash) and data truncation. Last 16 bits are kept.
     df[targetColumn] = df[targetColumn].apply(lambda x: hashlib.sha256(x.encode('utf-8')).hexdigest())
     df[targetColumn] = df[targetColumn].str.slice(-charsToKeep)
     df.to_csv('hashNtrunc.csv')  # optional output of hashed and truncated dataset for testing
@@ -27,48 +28,52 @@ def anonymityCheck(idColumn):
     return low_freq
 
 
-def dataCorrection(low_freq, breakingKLen):
+def dataCorrection(columnB, low_freq, breakingKLen):
     #Keep 1/k of the rows which break K anonymity and sort them in ascending order
     #Using the idea of slicing dataframe.
+    print('k value in dataCorrection is:' + str(k))
     if k==0:
         print('''k can't be set to 0. Please rest the k value''')
         exit(1)
     elif breakingKLen / k < 1:
-        savedDataItems = df[low_freq].sort_values(by=[low_freq]).head(1)
+        savedDataItems = df[low_freq].sort_values(by=[columnB]).head(1)                    #Case when the rows to keep is less than 1. Keeping 1 row
     else:
-        savedDataItems = df[low_freq].sort_values(by=[low_freq])[0:int(breakingKLen / k)]
-    savedDataItems.drop_duplicates()
-    savedDataItems.to_csv('candidToKeep.csv') #optional output for checking results
+        savedDataItems = df[low_freq].sort_values(by=[columnB])[0:int(breakingKLen / k)]   #Case when rows to keep > 1. Slice the dataset from row 0 to the index of breakingKlen/k
+    savedDataItems.drop_duplicates()                                                                #Remove potential duplicated rows from the rows to keep
+    savedDataItems.to_csv('candidToKeep.csv')                                                       #Optional output for checking 'rows to keep'
     return savedDataItems
 
 def dataReplication(candidRows):
     # Repeating the K-anonymity breaking rows by K times
     repeated = pd.concat([candidRows] * k, ignore_index=True)
+    print('k value in dataReplicaiton is:' + str(k))
     print('length of repeated is ' + str(len(repeated)))
     return repeated
 
 
 def finalOutput(datasetMain, datasetCorrection):
     # Concatenate the first two layers' outcome with the corrected dataset
-    frames = [datasetMain, datasetCorrection]
+    frames = [datasetMain, datasetCorrection]                                                       #The final processed dataset = Data which confirms to K-Anonymity + Processed K-anonymity breaking rows
     PDSE = pd.concat(frames)
     PDSE.to_csv('PDSE2.csv')
+    print('k here is: ' + str(k))
     print('length of PDSE is ' + str(len(PDSE)))
     print('SUM of cleanedMainSet + repeated is ' + str(len(datasetMain) + len(datasetCorrection)))
 
 
 def main():
+    print('k is now set to: ' + str(k))
     dataPreProcessing(identifierCol)
     kBreakingRows = anonymityCheck(identifierCol)
     kBreakingLen = len(df[kBreakingRows].index)
     print('The length of rows breaking K anonymity is: ' + str(kBreakingRows))
-    candidToKeep = dataCorrection(kBreakingRows, kBreakingLen)
+    candidToKeep = dataCorrection(colB, kBreakingRows, kBreakingLen)
     breakingToKeep = dataReplication(candidToKeep)
     cleanedMainSet = df.groupby(identifierCol).filter(lambda x: len(x) >= k)
     print('length of dataset without K breaking rows is ' + str(len(cleanedMainSet)))
     finalOutput(cleanedMainSet, breakingToKeep)
 
-
 while True:
+    df = pandas_zmq.recv_dataframe(socket)
     main()
 
