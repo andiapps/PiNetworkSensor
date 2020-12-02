7f525f40702846e2e9444f78d9f64a010b5d6d7d
Diiii <andyappdev@yahoo.com>
2020-12-02 12:50:41 +0100

    Update server.py
    Approach to concatenate each single data row collected from scanning and put them into one dataframe table

--- server.py
+++ server.py
@@ -18,18 +18,21 @@ def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.type == 0 and pkt.subtype == 4:
             if pkt.addr2 not in deviceList:
-                #deviceList.append(pkt.addr2)
+                deviceList.append(pkt.addr2)
                 #print("SSID: %s MAC addr: %s " %(pkt.info, pkt.addr2))
                 dFrame.append(pkt.addr2)
-                df = pd.DataFrame(dFrame, columns = ['MAC address'])
-                pandas_zmq.send_dataframe(socket, df)
-                return str(pkt.info), str(pkt.addr2)
+                #df = pd.DataFrame(dFrame, columns = ['MAC address'])
+                #pandas_zmq.send_dataframe(socket, df)
+                #print(deviceList)
+                #return str(pkt.info), str(pkt.addr2)
                 
-                
-while True:
-    time.sleep(1)
-    sniff(iface = "wlan0", prn = PacketHandler)   
-    #msg = sniff
-    #socket.send(msg)
-    time.sleep(5)
-    print("End of epoch")    
+               
+epoch = time.time() + 60   #set a fixed epoch length for detection
+while time.time() <= epoch:
+    sniff(iface = "wlan0", prn = PacketHandler)
+    df = pd.DataFrame(dFrame, columns = ['MAC address']) 
+    concatList = []
+    concatList.append(df)       #concatenate each data row to one dataframe
+    dfSum = pd.concat(concatList)
+    pandas_zmq.send_dataframe(socket, dfSum)    #send the concatenated dataframe through socket
+    print(dfSum)
